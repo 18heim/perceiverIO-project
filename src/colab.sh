1e8a1f4b9114ec1adbddc231545a4e2a0c8a15fa
18heim <christopher.heim@mines-paristech.fr>
2022-02-09 18:55:28 +0100

    [DEV] bash script for dataset download on colab, requirements, init

---
+++ src/colab.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Install dependencies
+echo "Install dependencies ..."
+pip install -r ./requirements.txt
+# Download dataset
+echo "Press 'q' to exit"
+echo "Press 'a' to download tiny-imagenet \n Press 'b' to download Sintel \n"
+count=0
+while : ; do
+read -n 1 k <&1
+if [[ $k = q ]] ; then
+printf "\nQuitting from the program\n"
+break
+# Set up tiny-imagenet
+elif [[ $k = a ]] ; then
+printf "\nDownloading tiny-imagenet ...\n"
+wget http://cs231n.stanford.edu/tiny-imagenet-200.zip -P /content/
+unzip /content/tiny-imagenet-200.zip
+break
+elif [[ $k = b ]] ; then
+printf "\nDownloading sintel\n"
+break
+else
+((count=$count+1))
+echo "Press 'q' to exit"
+fi
+done
+
+
